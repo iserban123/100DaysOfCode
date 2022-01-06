4ddd9dc6295cccde041144f061b155140cc442c2
Juliana S <48383802+iserban123@users.noreply.github.com>
2022-01-06 08:45:50 +0000

    link from replot

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ b = input("Whats the name of your fav pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
 print("Your band name is " + a + " " + b)
 #5. Make sure the input cursor shows on a new line, see the example at:
-#   https://replit.com/@appbrewery/band-name-generator-
+#   https://replit.com/@appbrewery/band-name-generator-app
