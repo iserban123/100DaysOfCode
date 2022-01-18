9d901862e15b8e4ff48b697e874df0985ff37e3c
eiulser <Parola*01>
2022-01-18 18:43:03 +0100

    day16 #TODO

--- Day16/main.py
+++ Day16/main.py
@@ -1,1 +1,30 @@
-print("sdsd")
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+genrep = CoffeeMaker()
+itema = Menu()
+bani = MoneyMachine()
+#val = MenuItem()
+
+itema.__init__()
+for x in itema.menu:
+    y = itema.menu("latte")
+    print(y)
+
+
+
+option = input(f"What do you like?{itema.get_items()}: ")
+print(itema.get_items())
+if option == "report":
+    genrep.report()
+else:
+    is_found = itema.find_drink(option)
+    yes_no = genrep.is_resource_sufficient(is_found)
+    if yes_no == True:
+        bani.process_coins()
+       # bani.make_payment(val.cost)
+        genrep.make_coffee(is_found)
+
+
+
+
