3eb87b60442de44f7d55c7fc38579d45568f686e
eiulser <Parola*01>
2022-01-20 15:15:44 +0100

    Random Walk3

--- Day18/main.py
+++ Day18/main.py
@@ -1,15 +1,24 @@
+import turtle
 from turtle import Turtle, Screen
 import random
 
-colors = ["black", "red", "purple", "yellow", "blue", "orange"]
+turtle.colormode(255)
 angle = [-90,0,90,180]
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("plum")
 timmy_the_turtle.speed(8)
+timmy_the_turtle.pensize(10)
 
-for _ in range(50):
- timmy_the_turtle.pen(pensize=10, pencolor=random.choice(colors))
+def color_change():
+   r = random.randint(0,255)
+   g = random.randint(0,255)
+   b = random.randint(0,255)
+   color_tuple = (r, g, b)
+   return color_tuple
+
+
+for _ in range(200):
+ timmy_the_turtle.color(color_change())
  timmy_the_turtle.forward(20)
  timmy_the_turtle.right(random.choice(angle))
 
