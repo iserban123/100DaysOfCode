834ae677eb393ad723eeed62a71c2989b662af90
eiulser <Parola*01>
2022-01-24 16:19:14 +0100

    snake

--- Day20/Snake_Game/snake.py
+++ Day20/Snake_Game/snake.py
@@ -1,34 +1,39 @@
 from turtle import Turtle
-MOVE=10
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 10
 UP = 90
 DOWN = 270
 LEFT = 180
 RIGHT = 0
 
+
 class Snake:
 
     def __init__(self):
-        self.ypos = 0
-        self.xpos = 0
-        self.snake_segments = []
+        self.segments = []
         self.create_snake()
-        self.head = self.snake_segments[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for i in range(3):
-            snake = Turtle(shape="square")
-            snake.color("white")
-            snake.penup()
-            snake.setposition(x=self.xpos, y=self.ypos)
-            self.xpos += MOVE
-            self.snake_segments.append(snake)
+        for z in STARTING_POSITIONS:
+            self.add_segment(z)
+
+    def add_segment(self, y):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(y)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
-        for snake_segment in range(len(self.snake_segments) - 1, 0, -1):
-            x_new = self.snake_segments[snake_segment-1].xcor()
-            y_new = self.snake_segments[snake_segment - 1].ycor()
-            self.snake_segments[snake_segment].goto(x_new, y_new)
-        self.head.forward(MOVE)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
         if self.head.heading() != DOWN:
@@ -36,22 +41,12 @@ class Snake:
 
     def down(self):
         if self.head.heading() != UP:
-             self.head.setheading(DOWN)
-
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.snake_segments[0].setheading(RIGHT)
+            self.head.setheading(DOWN)
 
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
-
-
-
-
-
-
-
-
-
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
