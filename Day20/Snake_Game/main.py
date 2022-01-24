b8b57357871d0ceb5acf42f9b210ac69ac5494f7
eiulser <Parola*01>
2022-01-24 23:02:04 +0100

    snake game

--- Day20/Snake_Game/main.py
+++ Day20/Snake_Game/main.py
@@ -34,4 +34,12 @@ while game_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280 :
         game_on = False
         score.game_over()
+
+    for seg in snake.segments[1:]:
+        if snake.head.distance(seg) < 10:
+            game_on = False
+            score.game_over()
+
+
+
 screen.exitonclick()
