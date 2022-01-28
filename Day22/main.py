e225358fb2938e886cf1c71e90ea5c0fe5493c21
eiulser <Parola*01>
2022-01-28 11:42:15 +0100

    files

--- Day22/main.py
+++ Day22/main.py
@@ -1,10 +1,27 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
-screen.setup(width=800, height=600)
+screen.setup(width=1000, height=800)
 screen.bgcolor("pink")
+screen.tracer(0)
+
+paddle_l = Paddle((470, 0))
+paddle_r = Paddle((-470, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(paddle_l.up, "p")
+screen.onkey(paddle_l.down, "m")
+screen.onkey(paddle_r.up, "a")
+screen.onkey(paddle_r.down, "b")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(1)
+    screen.update()
+    ball.move()
 screen.exitonclick()
-
-paddlex = Paddle(300, 0)
-
--- Day22/paddle.py
+++ Day22/paddle.py
@@ -3,10 +3,19 @@ from turtle import Turtle
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
-        self.shapesize(stretch_wid=1,stretch_len=4)
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.color("blue")
-        self.setposition(x=400,y=100)
+        self.penup()
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
 
